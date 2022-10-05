e6baad525432d0ac7c943253a2bb036f1a147338
KeisukeKonta <keisuke.konta@optim.co.jp>
2022-10-05 11:44:32 +0900

    :heavy_plus_sign: Add Pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { storeToRefs } from 'pinia'
+  import { useCounterStore } from '../store/counter'
+
+  const store = useCounterStore()
 
   defineProps<{ msg: string }>()
 
-  const count = ref(0)
+  const { count } = storeToRefs(store)
+  const { increment } = store
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="increment">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
